f06020770f708deef353a396e6d4a4edb365ebb4
Diego Rivas <darivas@uce.edu.ec>
2023-05-15 09:56:12 -0500

    first commit

---
+++ src/main/java/com/example/demo/PwU1P4TDr1Application.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4TDr1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4TDr1Application.class, args);
+		
+		System.out.print("Hola Spring sdfsdf");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
